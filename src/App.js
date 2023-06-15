8c5c898e192d42861000d25dc364031221582827
LimHaeJun <6462jun@gmail.com>
2023-06-15 13:50:36 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App(props) {
+function App(props) { //함수형 컴포넌트 생성
   const [backgroundColor, setBackgroundColor] = useState('');
 
   const handleChangeColor = (e) => {
     setBackgroundColor(e.target.value);
   };
 
-  return (
+  return ( //배경색 입력란 및 값 받기
     <div id="cell" style={{ backgroundColor }}>
       <span class="color">BackgroundColor: {backgroundColor}</span>
       <input type="text" value={backgroundColor} onChange={handleChangeColor} placeholder="색상을 입력하세요" />
