37cdcf9ab760b4ccd6e747e05970ab9ed8f4c5e2
LimHaeJun <6462jun@gmail.com>
2023-06-15 13:15:49 +0900

    App.js 내용 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [backgroundColor, setBackgroundColor] = useState('');
+
+  const handleChangeColor = (e) => {
+    setBackgroundColor(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor }}>
+      <p>BackgroundColor</p>
+      <input type="text" value={backgroundColor} onChange={handleChangeColor} placeholder="색상을 입력하세요" />
     </div>
   );
-}
+};
+
 
 export default App;
