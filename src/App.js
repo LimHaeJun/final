ba6ecfa682e164f2d016c2f81259366d6a1e6e86
LimHaeJun <6462jun@gmail.com>
2023-06-15 13:41:53 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const App = () => {
+function App(props) {
   const [backgroundColor, setBackgroundColor] = useState('');
 
   const handleChangeColor = (e) => {
@@ -9,8 +9,8 @@ const App = () => {
   };
 
   return (
-    <div style={{ backgroundColor }}>
-      <p>BackgroundColor</p>
+    <div id="cell" style={{ backgroundColor }}>
+      <span class="color">BackgroundColor: {backgroundColor}</span>
       <input type="text" value={backgroundColor} onChange={handleChangeColor} placeholder="색상을 입력하세요" />
     </div>
   );
